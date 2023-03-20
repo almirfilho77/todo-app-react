2f14526c3051b3173e94c8a4124ec402c9812769
Almir Filho <almir.filho77@gmail.com>
2023-03-19 20:30:39 -0400

    Add code relative to the tutorial until lesson 'React interactivity: Editing, filtering, conditional rendering'
        New functionality:
        - Edit a task through 'Edit' button
        - Apply filters through 'Filter' buttons

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter} />
+  ));
 
   function addTask(name) {
     const newTask = { id:`todo-${nanoid()}`, name, completed:false };
@@ -21,7 +38,7 @@ function App(props) {
         return {...task, completed:!task.completed};
       }
       return task;
-    })
+    });
     setTasks(updatedTasks);
   };
 
@@ -29,8 +46,21 @@ function App(props) {
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   };
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id)
+      {
+        return {...task, name:newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  };
   
-  const taskList = tasks?.map((task) => (
+  const taskList = tasks
+    ?.filter(FILTER_MAP[filter])
+    ?.map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
@@ -38,6 +68,7 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
 
@@ -49,9 +80,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form onSubmit={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton name="all"/>
-        <FilterButton name="Active"/>
-        <FilterButton name="Completed"/>
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
